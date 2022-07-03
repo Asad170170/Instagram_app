1e37b3a9db5cb23cad8d3a9c808ca5f494d0189b
Dev <dev@Devs-MacBook-Pro.local>
2022-07-03 14:51:49 +0500

    Implementing stories module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_155813) do
+ActiveRecord::Schema.define(version: 2022_07_02_223231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2022_06_30_155813) do
     t.string "username", limit: 20
     t.string "description"
     t.string "website"
+    t.boolean "is_public", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
