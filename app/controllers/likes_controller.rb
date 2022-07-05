59622c234ddc0a44ca0c11971c90016b456a7ac2
Dev <dev@Devs-MacBook-Pro.local>
2022-07-05 14:38:12 +0500

    bug fixes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,18 @@ class LikesController <ApplicationController
   def save_like
     @like=Like.new(post_id:params[:post_id],user_id: current_user.id )
     @post_id=params[:post_id]
+    existing_like=Like.where(post_id:@post_id,user_id: current_user.id)
+   
         respond_to do |format|
           format.js{
-              if @like.save
-                {success:true}
+              if existing_like.any?
+                #like already exist
+                existing_like.first.destroy
+                @success=false
+              elsif @like.save
+                @success=true
               else
-                {success:false}
+                @success=false
               end
             @post_likes=Post.find(@post_id).total_likes_count
             puts @post_likes
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,10 +2,15 @@ class Like <ApplicationRecord
   belongs_to :post
   belongs_to :user
   validates_uniqueness_of :post_id, scope: :user_id
-  after_create :update_post_likes_counter
+  after_create :update_post_likes_counter_increment
+  before_destroy :update_post_likes_counter_decrement
 
   private
-  def update_post_likes_counter
+  def update_post_likes_counter_increment
     Post.find(self.post_id).increment(:total_likes_count).save
+  end
+
+  def update_post_likes_counter_decrement
+    Post.find(self.post_id).decrement(:total_likes_count).save
   end
 end
